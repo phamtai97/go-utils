c83f1c0cc8bbb51ea1db4e36532ff1b0334cffc6
Tai. Pham Tinh Hong Tan <taiptht@vng.com.vn>
2021-09-11 15:14:46 +0700

    [doc] Add go doc

---
+++ doc.go
@@ -0,0 +1,7 @@
+/* 
+This package provides simple utils that help us quickly build projects. Currently, there are the following packages:
+- logger
+- ero
+*/
+
+package go_utils
---
+++ utils/error/doc.go
@@ -0,0 +1,64 @@
+/*
+The package provides a way to create errors.
+
+For example Usage
+
+import (
+	ero "github.com/phamtai97/go-utils/utils/error"
+	"github.com/phamtai97/go-utils/utils/logger"
+
+	"go.uber.org/zap"
+)
+
+func doError() error {
+	return ero.New("Failed to open file")
+}
+
+func main() {
+	logger.InitProduction("")
+
+	// err := ero.Newf("Not found file")
+	err := ero.New("Failed to open file")
+
+	// Add context
+	errA := err.AddContext("Call by component A")
+	errB := errA.AddContext("Call by component B")
+	errC := errB.AddContextf("Call by component C %d", 123)
+
+	// Create error by pre-existing error
+	errWrap := ero.Wrap(err)
+
+	// Add stackstrace
+	errWrapA := errWrap.AddStackTrace("Call by component wrapper A")
+	errWrapB := errWrapA.AddStackTrace("Call by component wrapperB")
+	errWrapC := errWrapB.AddStackTracef("Call by component wrapper C %d", 123)
+
+	// Logger error
+	logger.Error("Test", zap.Error(errC.Detail()))
+	logger.Error("Test", zap.Error(errWrapC.Detail()))
+	logger.Error("Test", zap.Error(errC))
+	logger.Error("Test", zap.Error(errWrapC))
+	logger.Error("Test", zap.String("Failed", errA.Error()))
+	logger.Error("Test", zap.String("Failed", errWrapA.Error()))
+
+	// Check error with Is
+	if errC.Is(errA) {
+		logger.Info("Error C is error A", zap.String("ErrorC", errC.Error()), zap.String("ErrorA", errA.Error()))
+	}
+
+	if !errWrap.Is(err) {
+		logger.Info("errWrap is not err", zap.String("errWrap", errWrap.Error()), zap.String("err", err.Error()))
+	}
+
+	// Get root cause of error
+	logger.Error("Root cause", zap.String("Root cause", errC.RootCause().Error()))
+	logger.Error("Root cause", zap.String("Root cause", errC.RootCauseStr()))
+	logger.Error("Root cause", zap.Error(errC.RootCause().Detail()))
+
+	// Case error to ErrorWrapper
+	errCast := doError().(*ero.ErrorWrapper)
+	logger.Error("Cast error to ErrorWrapper", zap.Error(errCast.Detail()))
+}
+*/
+
+package ero
