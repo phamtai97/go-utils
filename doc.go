17562819afe916901704ad5d3dd99872e729fec3
Tai. Pham Tinh Hong Tan <taiptht@vng.com.vn>
2021-09-12 11:37:10 +0700

    [doc] update godoc

--- doc.go
+++ doc.go
@@ -1,6 +1,5 @@
 // This package provides simple utils that help us quickly build projects. Currently, there are the following packages:
-// - logger
-// - ero
-// - datetime
-
+//	- logger
+//	- ero
+//	- datetime
 package utils
